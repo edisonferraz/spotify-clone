22ebf53eccaaf89169e585a04e99a83c0ac65150
Edison <edisonferraz@gmail.com>
2019-01-25 18:22:51 +0100

    Carregando playlists na sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,68 +1,105 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as PlaylistsActions } from '../../store/ducks/playlists';
 
 import { Container, NewPlaylist, Nav } from './styles';
 
 import AddPlaylistIcon from '../../assets/images/add_playlist.svg';
 
-const Sidebar = () => (
-  <Container>
-    <div>
-      <Nav main>
-        <li>
-          <a href="">Navegar</a>
-        </li>
-        <li>
-          <a href="">Rádio</a>
-        </li>
-      </Nav>
+class Sidebar extends Component {
+  static propTypes = {
+    getPlaylistRequest: PropTypes.func.isRequired,
+    playlists: PropTypes.shape({
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          title: PropTypes.string,
+        }),
+      ),
+    }).isRequired,
+  };
 
-      <Nav>
-        <li>
-          <span>Sua Biblioteca</span>
-        </li>
-        <li>
-          <a href="">Seu Daily Mix</a>
-        </li>
-        <li>
-          <a href="">Tocados recentemente</a>
-        </li>
-        <li>
-          <a href="">Músicas</a>
-        </li>
-        <li>
-          <a href="">Albums</a>
-        </li>
-        <li>
-          <a href="">Artistas</a>
-        </li>
-        <li>
-          <a href="">Estações</a>
-        </li>
-        <li>
-          <a href="">Arquivos locais</a>
-        </li>
-        <li>
-          <a href="">Vídeos</a>
-        </li>
-        <li>
-          <a href="">Poscasts</a>
-        </li>
-      </Nav>
+  componentDidMount() {
+    this.props.getPlaylistRequest();
+  }
 
-      <Nav>
-        <li>
-          <span>Playlist</span>
-        </li>
-        <li>
-          <a href="">Melhores do rock</a>
-        </li>
-      </Nav>
-    </div>
-    <NewPlaylist>
-      <img src={AddPlaylistIcon} alt="Adicionar Playlist" />
-      Nova playlist
-    </NewPlaylist>
-  </Container>
-);
+  render() {
+    return (
+      <Container>
+        <div>
+          <Nav main>
+            <li>
+              <a href="">Navegar</a>
+            </li>
+            <li>
+              <a href="">Rádio</a>
+            </li>
+          </Nav>
 
-export default Sidebar;
+          <Nav>
+            <li>
+              <span>Sua Biblioteca</span>
+            </li>
+            <li>
+              <a href="">Seu Daily Mix</a>
+            </li>
+            <li>
+              <a href="">Tocados recentemente</a>
+            </li>
+            <li>
+              <a href="">Músicas</a>
+            </li>
+            <li>
+              <a href="">Albums</a>
+            </li>
+            <li>
+              <a href="">Artistas</a>
+            </li>
+            <li>
+              <a href="">Estações</a>
+            </li>
+            <li>
+              <a href="">Arquivos locais</a>
+            </li>
+            <li>
+              <a href="">Vídeos</a>
+            </li>
+            <li>
+              <a href="">Poscasts</a>
+            </li>
+          </Nav>
+
+          <Nav>
+            <li>
+              <span>Playlist</span>
+            </li>
+            {this.props.playlists.data.map(playlist => (
+              <li key={playlist.id}>
+                <Link to={`playlists/${playlist.id}`}>{playlist.title}</Link>
+              </li>
+            ))}
+          </Nav>
+        </div>
+        <NewPlaylist>
+          <img src={AddPlaylistIcon} alt="Adicionar Playlist" />
+          Nova playlist
+        </NewPlaylist>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  playlists: state.playlists,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(PlaylistsActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Sidebar);
